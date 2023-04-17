2cf32f1424883adf65e6c179a7619beea1d79f05
lenidar <55443074+lenidar@users.noreply.github.com>
2023-04-17 15:26:32 +0800

    Add project files.

---
+++ FileReaderDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReaderDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "";
+            using (StreamReader sr  = new StreamReader("sample3.sectionA"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //Console.WriteLine(line);
+                    foreach(char l in line)
+                    {
+                        Console.Write((int)l + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
